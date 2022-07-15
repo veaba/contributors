96f7c105ea2b54246e0aed0d62d8633e5d4e6516
veaba <godpu@outlook.com>
2022-07-16 02:32:05 +0800

    chore: update title

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader({
     defaultImport: 'raw'
