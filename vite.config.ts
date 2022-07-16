381ad420ac2f9d64c609895f82b21205173d2ad3
veaba <godpu@outlook.com>
2022-07-16 10:49:45 +0800

    chore: add vitest example

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import svgLoader from 'vite-svg-loader'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  test: {},
   plugins: [vue(), svgLoader({
     defaultImport: 'raw'
   })],
